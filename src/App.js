445f43f48da0e55390ea897c2e93ecdf04d52c94
Henry Zhou <zhou96@illinois.edu>
2021-10-04 17:32:07 -0500

    1. Basic login/logout/register with no confirm password check. 2. Delete seems to work
    TODO: Get toggle complete and create to work.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,38 @@ function App() {
    
 
     
+    function toDoReducer (state, action) {
+        switch (action.type) {
+            case 'CREATE_TODO':
+              const newToDo = { 
+                  title: action.title,
+                  description: action.description,
+                  dateCreated: action.dateCreated,
+                  complete: false,
+                  dateCompleted: ''
+                }
+                return [ newToDo, ...state ]
+            case 'TOGGLE_TODO':
+              return state.map(
+                  (todo, i)=>{
+                      if(i === action.index){
+                          return {... todo, dateCompleted: action.completed?Date.now():'', complete: action.completed } 
+                        } else {
+                            return todo
+                        }
+                    })
+              
+              
+            case 'DELETE_TODO':
+                return state.filter((todo,i)=>{return i!==action.index})
+            default:
+               return state;
+        }
+      }
+
+
+    const [toDoList, dispatchToDo] = useReducer(toDoReducer,[{title: 'Do something', description: 'something', dateCreated: Date.now(), complete: false, dateCompleted: ''}]);
+
     
     function userReducer (state, action) {
         switch (action.type) {
@@ -27,7 +59,7 @@ function App() {
         <div>
             <UserLine username={username} dispatchUser={dispatchUser}/>
 			{username && <AddToDo username={username}/>}
-			<ToDoList/>
+			<ToDoList toDoList={toDoList} dispatchToDo={dispatchToDo}/>
         </div>
   );
 }
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,15 +1,24 @@
 
 
-function ToDo({title='', description='', dateCreated=Date.now(), complete=false, dateCompleted=Date.now()}) {
+function ToDo({title='', description='', dateCreated=Date.now(), complete=false, listIndex, dateCompleted=Date.now(), dispatchToDo}) {
+    const index = listIndex;
+
+	function handleDelete(evt){
+        dispatchToDo({type:"DELETE_TODO", index})
+	}
 	
+	function handleToggle(evt){
+        dispatchToDo({type: "TOGGLE_TODO", index, complete: evt.target.checked})
+	}
     return (
         <div>
 	        <h3>{title}</h3>
 		    <p>{description}</p>
 		    <p>This todo was created on {dateCreated}</p>
 		    <p>This todo was completed on {dateCompleted}</p>
-		    <input  type="checkbox"/>
-	</div>
+		    <input  type="checkbox" onClick={handleToggle}/>
+			<button type="button" onClick={handleDelete}>Delete</button>
+	    </div>
   );
 }
 
